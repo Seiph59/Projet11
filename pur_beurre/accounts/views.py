4cb04ce05c50507adbe9c3180c52545cbe018cd8
Damien G <chimere_62@hotmail.fr>
2019-07-19 21:53:33 +0200

    email_confirmed status updated when user confirms his email

--- pur_beurre/accounts/views.py
+++ pur_beurre/accounts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.template.loader import render_to_string
+from accounts.models import Profile
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .tokens import account_activation_token
 from accounts.forms import UserRegisterForm
@@ -56,8 +57,9 @@ def activate(request, uidb64, token):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
-        user.profile.email_confirmed = True
-        user.save()
+        profile = Profile.objects.get(user=uid)
+        profile.email_confirmed = True
+        profile.save()
         login(request, user)
         messages.success(request, ('Votre compte est validé.'))
         return redirect('welcome:home')
