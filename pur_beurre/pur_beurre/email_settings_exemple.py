5bdb2d6c81126f0d5bae48b6d876a7ce180b20b2
Damien G <chimere_62@hotmail.fr>
2019-07-27 13:52:11 +0200

    exemple for email settings added

---
+++ pur_beurre/pur_beurre/email_settings_exemple.py
@@ -0,0 +1,8 @@
+""" Settings File exemple to set up the email parameters """
+
+HOST = 'smtp.email.com'
+PORT = 534
+HOST_USER = 'your@email.com'
+HOST_PASSWORD = 'your password'
+USE_TLS = True
+# USE_SSL
